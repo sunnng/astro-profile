406fcfb7496740332245b028cbfaf319ecff3dde
sunnng <61936492+sunnng@users.noreply.github.com>
2023-10-11 14:29:18 +0800

    配置 github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sunnng.github.io",
+  base: "/astro-profile",
+});
